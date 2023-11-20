f6e9cbe6178839bdce6b4433fe3229c867c1bc14
Captain1014 <leahchung99@gmail.com>
2023-11-20 15:12:02 -0500

    update edit list

--- app/api/lists/[id]/route.js
+++ app/api/lists/[id]/route.js
@@ -10,23 +10,34 @@ export async function PUT(request, { params }) {
   const { newKorean: korean, newMeaning: meaning } = await request.json();
   // await connectMongoDB();
   await List.findByIdAndUpdate(id, { korean, meaning });
-  return NextResponse.json({ message: "List updated" }, { status: 200 });
+  return NextResponse.json(
+    { message: "List updated" },
+    { status: 200 },
+
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    }
+  );
 }
 
 export async function GET(request, { params }) {
-    const { id } = params;
-    // await connectMongoDB();
-    const list = await List.findOne({_id: id});
-    return NextResponse.json({list}, { status: 200 }, 
-      
-        {
-          headers: {
-            "Access-Control-Allow-Origin":  "*",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type",
-          },
-        }
-      
-      );
-  }
-  
+  const { id } = params;
+  // await connectMongoDB();
+  const list = await List.findOne({ _id: id });
+  return NextResponse.json(
+    { list },
+    { status: 200 },
+
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    }
+  );
+}
