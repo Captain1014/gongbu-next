a0f2ca062d3674a48bf6b3815ff32be3e7b10dd8
Captain1014 <leahchung99@gmail.com>
2023-11-19 22:57:02 -0500

    no dashboard/userinfo  for now

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,9 @@
 
-import UserInfo from "@/components/UserInfo";
-export default function Dashboard() {
-  return (
+// import UserInfo from "@/components/UserInfo";
+// export default function Dashboard() {
+//   return (
    
-      <UserInfo />
+//       <UserInfo />
    
-  );
-}
+//   );
+// }
