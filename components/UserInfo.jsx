f769fba01bf3a987f8466abd649d99c2d8f82a56
Captain1014 <leahchung99@gmail.com>
2023-12-04 12:33:52 -0500

    change singOut

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -5,7 +5,10 @@ import { useSession } from "next-auth/react";
 
 export default function UserInfo() {
   const { data: session } = useSession();
-
+  const handleLogout = async () => {
+    await signOut();
+    // Additional logic if needed
+  };
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
@@ -16,7 +19,7 @@ export default function UserInfo() {
           Email: <span className="font-bold">{session?.user?.email}</span>
         </div>
         <button
-          onClick={() => signOut()}
+          onClick={handleLogout}
           className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
         >
           Log Out
