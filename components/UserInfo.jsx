d2a0a60eafad74f48f9dd9675c5f3a2e7e599e8f
Captain1014 <leahchung99@gmail.com>
2023-12-04 13:27:56 -0500

    fix dashboard

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -3,7 +3,7 @@
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
+import LoginForm from "./LoginForm";
 
 export default function UserInfo() {
   const { data: session } = useSession();
@@ -17,27 +17,30 @@ export default function UserInfo() {
       await router.push("/");
     } else {
       await signOut();
-      
     }
     // Additional logic if needed
   };
 
   return (
     <div className="grid place-items-center h-screen">
-      <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
-        <div>
-          Name: <span className="font-bold">{session?.user?.name}</span>
+      {session && (
+        <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
+          <div>
+            Name: <span className="font-bold">{session?.user?.name}</span>
+          </div>
+          <div>
+            Email: <span className="font-bold">{session?.user?.email}</span>
+          </div>
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
+          >
+            Log Out
+          </button>
         </div>
-        <div>
-          Email: <span className="font-bold">{session?.user?.email}</span>
-        </div>
-        <button
-          onClick={handleLogout}
-          className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
-        >
-          Log Out
-        </button>
-      </div>
+      )}
+
+      {!session && <LoginForm />}
     </div>
   );
 }
